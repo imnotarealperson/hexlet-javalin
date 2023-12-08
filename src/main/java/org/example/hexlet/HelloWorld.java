eda7fc92ba05370de2129a1aa18cbe06caa362d2
idgadsf <idgadsf@gmail.com>
2023-12-08 04:19:52 +0300

    add /hello path processing

--- src/main/java/org/example/hexlet/HelloWorld.java
+++ src/main/java/org/example/hexlet/HelloWorld.java
@@ -1,6 +1,7 @@
 package org.example.hexlet;
 
 import io.javalin.Javalin;
+import org.eclipse.jetty.util.StringUtil;
 
 public class HelloWorld {
     public static void main(String[] args) {
@@ -10,6 +11,10 @@ public class HelloWorld {
         });
         // Описываем, что загрузится по адресу /
         app.get("/", ctx -> ctx.result("Hello World"));
+        app.get("/hello", ctx -> ctx.result(String.format("Hello, %s!", !StringUtil.isBlank(ctx.queryParam("name"))
+                                                                ? ctx.queryParam("name")
+                                                                : "World")));
+
         app.start(7070); // Стартуем веб-сервер
     }
 }
